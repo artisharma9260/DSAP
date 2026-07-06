14a59e9fc5773750b72bc49116013fe90b3e012f
artisharma9260 <poojapooja2780@gmail.com>
2026-07-06 06:24:25 +0000

    text

--- Arrays/MajorityElement/optimal.cpp
+++ Arrays/MajorityElement/optimal.cpp
@@ -7,17 +7,17 @@ public:
         int cnt = 0;
         int el = 0;
         for(int i = 0; i < n; i++){
-            if(count == 0){
-                el = arr[i];
+            if(cnt == 0){
+                el = nums[i];
                 cnt = 1;
             }
-            else if(el == arr[i]) cnt++;
+            else if(el == nums[i]) cnt++;
             else cnt--;
 
         }
     int cnt1 = 0;
     for(int i = 0; i < n; i++){
-        if(arr[i] == el) cnt1++;
+        if(nums[i] == el) cnt1++;
     }
     if(cnt1 > n / 2){
         return el;
