2dd0af3f9403b4cdb85b1f6684719db1c103811d
artisharma9260 <poojapooja2780@gmail.com>
2026-08-29 10:58:14 +0000

    greedy algorithm

---
+++ GreedyAlgo/lemonadechange.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+class LemonadeStand{
+public:
+    bool lemonadechange(vector<int>& bills){
+        int five = 0;
+        int ten = 0;
+        for(int bill : bills){
+            if(bill == 5){
+                five++;
+            }
+            else if(bill == 10){
+                if(five > 0){
+                    five--;
+                    ten++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(five > 0 && ten > 0){
+                    five--;
+                    ten--;
+                }
+                else if(five >=3){
+                    five -= 3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+int main() {
+    vector<int> bills = {5, 5, 5, 10, 20};
+
+    cout << "Queue of customers: ";
+    for (int bill : bills) cout << bill << " ";
+    cout << endl;
+
+    LemonadeStand stand;
+    bool ans = stand.lemonadechange(bills);
+
+    if (ans)
+        cout << "It is possible to provide change for all customers." << endl;
+    else
+        cout << "It is not possible to provide change for all customers." << endl;
+
+    return 0;
+}
