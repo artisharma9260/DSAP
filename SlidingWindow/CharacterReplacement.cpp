2d8ae67c59516466a8050e58d88f6af148df8d07
artisharma9260 <poojapooja2780@gmail.com>
2026-09-03 10:29:24 +0000

    modified

--- SlidingWindow/CharacterReplacement.cpp
+++ SlidingWindow/CharacterReplacement.cpp
@@ -5,8 +5,8 @@ public:
     int characterReplacement(string s,int k){
         vector<int> freq(26,0);
         int left = 0,right = 0;
-        int maxCount = 0;
-        int maxLength = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
         while(right < s.size()){
             freq[s[right] - 'A']++;
             maxFreq = max(maxFreq,freq[s[right]]);
